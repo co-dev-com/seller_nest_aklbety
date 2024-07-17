11ae663d5d782978c831482e5fb88b3dfde1f5b5
Yousief Noaman <yousief784@gmail.com>
2024-07-17 23:03:48 +0300

    Setup i18n

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { I18nContext, I18nService } from 'nestjs-i18n';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly i18n: I18nService,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.i18n.t('lang.HELLO', {
+      args: { name: 'Toon', opinion: 'Terrible' },
+      lang: I18nContext.current().lang,
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../config/configuration';
+import * as path from 'path';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
@@ -10,6 +12,17 @@ import { PrismaModule } from './prisma/prisma.module';
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
+    }),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        fallbackLanguage: configService.getOrThrow('FALLBACK_LANGUAGE'),
+        loaderOptions: {
+          path: path.join(__dirname, '/i18n/'),
+          watch: true,
+        },
+      }),
+      resolvers: [{ use: QueryResolver, options: ['lang'] }, AcceptLanguageResolver, new HeaderResolver(['x-lang'])],
+      inject: [ConfigService],
     }),
     PrismaModule,
   ],
